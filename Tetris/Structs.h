1050752eef2412bf62500e26aa0594ab430aec50
Maxime Dedobbeleer <maxime.dedobbeleer@gmail.com>
2018-11-14 09:12:37 +0100

    Added base scripts

---
+++ Tetris/Structs.h
@@ -0,0 +1,22 @@
+#pragma once
+struct Point2f
+{
+	float x;
+	float y;
+};
+
+struct Rectf
+{
+	float left;
+	float bottom;
+	float width;
+	float height;
+};
+
+struct Color4f
+{
+	float r;
+	float g;
+	float b;
+	float a;
+};
